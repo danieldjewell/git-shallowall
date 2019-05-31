6a0d95d33a23f1a8df353d1bc8d2867758b1b60b
Daniel Jewell <23286051+danieldjewell@users.noreply.github.com>
2019-05-30 18:02:26 -0700

    Initial Commit
    Initial commit with license, readme, and script

---
+++ README.md
@@ -0,0 +1,45 @@
+### About
+
+I was using [SpaceVim](https://github.com/SpaceVim/SpaceVim) and noticed that the plugin manager was cloning 
+git repositories without using the ```--depth``` option on ```git clone```. 
+The end result, of course, is that you end up with a complete git repository (when you might not want/need one). 
+
+I also thought about the number of times I had done a non-shallow clone and how it would be nice to have a way to clean that up.
+
+### Install/Usage
+
+Copying the script to a location in your ```bash $PATH``` would be preferable - I tend to like places like ```/usr/local/sbin/ or /usr/local/bin```. Or perhaps another directory where you keep your favorite scripts :grin:. 
+
+If in your PATH, simply run the script -- ```git-shallowdepth1.sh```. As explained below, if the current directory is a git repo, it will operate on that directory (and all submodules of that repo). If not, it will search *from the current directory* for all directories below that contain a .git directory (e.g. all repos beneath the current directory). 
+
+### Warning
+
+Be really extra careful running this in high level directories... Running it in / could produce some very undesirable results. 
+
+### Purpose/Function
+
+As of the initial commit, this is just a simple bash script that, when run:
+
+* Checks the current directory for a ".git" directory (e.g. is the current dir a git repository)
+* If yes, confirms before continuing - then shallows the git repo to a depth of 1
+* It then checks whether or not there are submodules
+* If yes, runs the cleanup commands recurisvely on all submodules using the ```git submodule foreach``` command
+
+If the current directory is ***not*** a git repository (e.g. no ".git" directory was found): 
+
+* Runs a ```find``` for all directories beneath the current directory that contain a ".git" directory
+* Confirms with the user whether or not to proceed
+* Loops through the above code (sans individual confirmation) and runs the shallow operation on each directory found
+
+Additionally, the script totals space used before/after and the difference saved for each repo -- and outputs it at the end. 
+
+### Todo
+
+[ ] Some additional warnings/checks perhaps? Like "if pwd is /, put up red and yellow flashing text and ask for double secret confirmation before proceeding"
+[ ] Probably double check to make sure that all of the git cleanup commands are necessary (could be overlap)
+[ ] More documentation (e.g. --help)
+[ ] Options for depth control (e.g. specify a depth to use or a date)
+[ ] Error Checking / Prettier Output
+[ ] Test on more systems (Initial version was written on Termux/Android)
+
+
---
+++ git-shallowdepth1.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+# Copyright (C) 2019 Jewell, Daniel <https://github.com/danieldjewell/>
+# Author: Jewell, Daniel <https://github.com/danieldjewell/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is quick and dirty and might break things.
+
+
+# run git commands on the current directory to prune to depth=1 and for all submodules too 
+
+declare -i GLO_SIZE_BEFORE=0
+declare -i GLO_SIZE_AFTER=0
+declare -i GLO_SIZE_DIFF=0
+
+
+confirm() {
+# call with a prompt string or use a default
+read -r -p "${1:-Are you sure? [y/N]} " response
+
+case "$response" in
+  [yY][eE][sS]|[yY])
+    echo 1
+    ;;
+  *)
+    echo 0
+    ;;
+esac
+  
+  
+}
+
+
+pruneGitDir() 
+{
+cd $1
+sizeBeforeBytes=$(du -sb $1 | cut -f1)
+echo "Shallowing $1"
+
+echo "GIT: Running FETCH to Depth=1"
+git fetch --depth=1 -pP 
+
+echo "GIT: Reflog expire all "
+git reflog expire --expire=0 --all
+sleep 2s
+
+echo "GIT: Prune"
+git prune --progress
+
+echo "GIT: Prune-packed"
+git prune-packed
+
+echo "GIT: Repack (-adfF)"
+git repack -adfF
+
+echo "GIT: GC (--aggressive)"
+git gc --aggressive 
+sleep 2s
+
+
+gsm=$(git submodule)
+gsms=$(git submodule | wc -l)
+if [[ $gsms -gt 0 ]]; then 
+  #there is 1 or more submodule to address
+  #git submodule foreach --recursive 'git-fetch --depth=1 -P'
+  #git submodule foreach --recursive 'git-reflog expire --expire=0 --all' 
+  #git submodule foreach --recursive 'git-gc --aggressive'
+  sleepTime=$(($gsms * 4))
+  echo ".:.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:."
+  echo "+ + + + + + + + + + + + + + + + + + +"
+  echo "+  S U B M O D U L E S   F O U N D  +"
+  fecho "+-----------------------------------+"
+  echo "Cleaning $gsms submodules"
+  echo "Pruning/Fetch to Depth=1"
+  git submodule foreach --recursive 'git-fetch --depth=1 -pP' 
+  sleep $sleepTime
+  echo "Reflog: Expire"
+  git submodule foreach --recursive 'git-reflog expire --expire=0 --all'
+  sleep $sleepTime
+  echo "Prune"
+  git submodule foreach --recursive 'git-prune --progress'
+  sleep $sleepTime
+  echo "Prune Packed"
+  git submodule foreach --recursive 'git-prune-packed'
+  sleep $sleepTime
+  echo "Repack -adfF"
+  git submodule foreach --recursive 'git-repack -adfF'
+  sleep $sleepTime
+  echo "git-gc: --aggressive"
+  git submodule foreach --recursive 'git-gc --aggressive'
+  sleep $sleepTime
+  echo "Submodules Cleaned"
+fi
+
+sizeAfterBytes=$(du -sb $1 | cut -f1)
+diffBytes=$(($sizeBeforeBytes - $sizeAfterBytes))
+
+echo "Done cleaning $1"
+echo "Size before: $sizeBeforeBytes"
+echo "Size after: $sizeAfterBytes"
+echo "Savings: $diffBytes"
+
+GLO_SIZE_BEFORE+=sizeBeforeBytes
+GLO_SIZE_AFTER+=sizeAfterBytes
+GLO_SIZE_DIFF+=diffBytes
+}
+
+findAllGitReposFromCD()
+{
+  pwdFull=$(pwd -P)
+  #IFS='\n'
+  readarray allGits < <(find $pwdFull -type d -name '.git' -printf '%h\n')
+  gitQty="${#allGits[*]}"
+  #echo ${#allGits[@]}
+  #echo ${#allGits[*]}
+  #echo ${#allGits}
+
+  echo "Found $gitQty different repositories"
+  echo "Preparing to prune all repos"
+  conf=$(confirm)
+  # echo $conf
+  if [[ $conf -eq 1 ]]; then
+
+    for g in ${allGits[@]}; do
+      echo $g
+      pruneGitDir $g
+      echo "----------"
+      echo "Current Summary"
+      echo "Total Bytes Before: $GLO_SIZE_BEFORE"
+      echo "Total Bytes After: $GLO_SIZE_AFTER"
+      echo "Total Bytes Saveed: $GLO_SIZE_DIFF"
+    done
+    
+    echo "++++++++++"
+    echo "Done!"
+    echo "Total Command Summary"
+    echo "Total Bytes Before: $GLO_SIZE_BEFORE"
+    echo "Total Bytes After: $GLO_SIZE_AFTER"
+    echo "Total Bytes Saveed: $GLO_SIZE_DIFF"
+    echo "++++++++++++++++++++++++++++++++++"
+  fi
+
+}
+
+
+cwd=$(pwd)
+if [[ ! -d $cwd/.git ]]; then 
+  echo "Not a git repository"
+  echo "finding all git repos from the cwd"
+  findAllGitReposFromCD
+  # need to do some looping
+else
+  echo "Current Directory is a GIT Repository"
+  echo "Will prune $cwd"
+  conf=$(confirm)
+  if [[ $conf -eq 0 ]]; then 
+    echo "Action cancelled."
+    exit
+  fi
+  
+  sizeBeforeBytes=$(du -sb $cwd | cut -f1)
+  pruneGitDir $cwd
+  sizeAfterBytes=$(du -sb $cwd | cut -f1)
+  diffBytes=$(($sizeBeforeBytes - $sizeAfterBytes))
+  
+  #GLO_SIZE_BEFORE+=sizeBeforeBytes
+  #GLO_SIZE_AFTER+=sizeAfterBytes
+  #GLO_SIZE_DIFF+=diffBytes
+
+  #echo "Done cleaning $cwd"
+  #echo "Size before: $sizeBeforeBytes"
+  #echo "Size after: $sizeAfterBytes"
+  #echo "Savings: $diffBytes"
+
+  echo "----------"
+  echo "Current Summary"
+  echo "Total Bytes Before: $GLO_SIZE_BEFORE"
+  echo "Total Bytes After: $GLO_SIZE_AFTER"
+  echo "Total Bytes Saveed: $GLO_SIZE_DIFF"
+  echo "----------------------------------"
+
+fi 
+
+
+
+
+
+
